283e66991d45891a71caffaada32b81a29052aab
Edson Martin Feitosa <edsonmfeitosa@hotmail.com>
2022-03-10 22:35:34 -0300

    trabalhando com tempo

--- Jogos3d202201/Assets/Scripts/Pontuacao.cs
+++ Jogos3d202201/Assets/Scripts/Pontuacao.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pontuacao : MonoBehaviour
 {
     private int quantidade = 0;
+    public float tempo = 0;
+    public Text txtTempo;
     void Start()
     {
         
@@ -13,6 +16,8 @@ public class Pontuacao : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        tempo += Time.deltaTime;
+        txtTempo.text = "Tempo: " + tempo.ToString("00.00");
         
     }
     private void OnTriggerEnter(Collider other)
